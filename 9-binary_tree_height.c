50821768fa638724df534720bee564c370be8fb2
mmanyuat <mmanyuat201@gmail.com>
2024-03-27 23:39:29 -0700

    push

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,18 +8,13 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t height = 0;
+	size_t left_height, right_height;
 
 	if (tree == NULL)
 	return (0);
 
-	while (tree && (tree->left || tree->right))
-	{
-	if (tree->left)
-	tree = tree->left;
-	else
-	tree = tree->right;
-	height++;
-	}
-	return (height);
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+
+	return (1 + (left_height > right_height ? left_height : right_height));
 }
